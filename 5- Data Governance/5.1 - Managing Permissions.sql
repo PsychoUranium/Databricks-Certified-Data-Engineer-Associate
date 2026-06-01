9d1aeac61c5cfdf8372f221723c9142af70962c9
PsychoUranium <ai.stackq@gmail.com>
2026-06-01 07:20:37 +0000

    FIX

--- 5- Data Governance/5.1 - Managing Permissions.sql
+++ 5- Data Governance/5.1 - Managing Permissions.sql
@@ -1,9 +1,8 @@
-CREATE DATABASE IF NOT EXISTS hive_metastore.hr_db
-LOCATION 'dbfs:/mnt/demo/hr_db.db';
+CREATE SCHEMA IF NOT EXISTS dbricks.hr_db;
 
-CREATE TABLE hive_metastore.hr_db.employees (id INT, name STRING, salary DOUBLE, city STRING);
+CREATE OR REPLACE TABLE dbricks.hr_db.employees (id INT, name STRING, salary DOUBLE, city STRING);
 
-INSERT INTO hive_metastore.hr_db.employees
+INSERT INTO dbricks.hr_db.employees
 VALUES (1, "Anna", 2500, "Paris"),
        (2, "Thomas", 3000, "London"),
        (3, "Bilal", 3500, "Paris"),
@@ -12,17 +11,16 @@ VALUES (1, "Anna", 2500, "Paris"),
        (6, "Adam", 3500, "London"),
        (7, "Ali", 3000, "Paris");
 
-CREATE VIEW hive_metastore.hr_db.paris_emplyees_vw
-AS SELECT * FROM hive_metastore.hr_db.employees WHERE city = 'Paris';
-
+CREATE VIEW dbricks.hr_db.paris_emplyees_vw
+AS SELECT * FROM dbricks.hr_db.employees WHERE city = 'Paris';
 ------------------------------------------------------
 
-GRANT SELECT, MODIFY, READ_METADATA, CREATE ON SCHEMA hive_metastore.hr_db TO hr_team;
+GRANT SELECT, MODIFY, CREATE ON SCHEMA dbricks.hr_db TO hr_team;
 
-GRANT USAGE ON SCHEMA hive_metastore.hr_db TO hr_team;
+GRANT USAGE ON SCHEMA dbricks.hr_db TO hr_team;
 
-GRANT SELECT ON VIEW hive_metastore.hr_db.paris_emplyees_vw TO `adam@derar.cloud`;
+GRANT SELECT ON VIEW dbricks.hr_db.paris_emplyees_vw TO `adam@derar.cloud`;
 
-SHOW GRANTS ON SCHEMA hive_metastore.hr_db;
+SHOW GRANTS ON SCHEMA dbricks.hr_db;
 
-SHOW GRANTS ON VIEW hive_metastore.hr_db.paris_emplyees_vw;
+SHOW GRANTS ON VIEW dbricks.hr_db.paris_emplyees_vw;
